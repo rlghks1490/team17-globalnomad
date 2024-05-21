098ef35723dd1528c4a7e1ada7bb390ceaae182f
KimTaeEun1632 <qpwoal1324@naver.com>
2024-05-21 16:34:13 +0900

    feat: input 작업중

--- pages/LoginPage/login.tsx
+++ pages/LoginPage/login.tsx
@@ -1,1 +1,24 @@
-1;
+import Input from "@/Components/Input/Input";
+
+const login = () => {
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex flex-col gap-8 justify-center items-center w-full h-full p-4">
+        <div className="flex flex-wrap gap-4 justify-center">
+          <Input label="First Name" placeholder="Sylvester" />
+          <Input label="Second Name" placeholder="Stallone" />
+          <Input defaultValue="I am default value" label="I am label" />
+          <Input label="No placeholder" />
+        </div>
+        <div className="flex flex-wrap gap-4 justify-center">
+          <Input label="Disabled, No placeholder" disabled />
+          <Input value="I am value. Disabled." disabled />
+          <Input value="I am label" disabled />
+          <Input label="I am Label" value="I am label" disabled />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
