af11f5b8df8bf9d6cad182692bb6cb20e4994879
KimTaeEun1632 <qpwoal1324@naver.com>
2024-05-21 17:49:39 +0900

    chore: login Input 수정

--- pages/LoginPage/login.tsx
+++ pages/LoginPage/login.tsx
@@ -3,16 +3,16 @@ import Input from "@/Components/Input/Input";
 const login = () => {
 
   return (
+
+    
     <form>
       <Input 
-        state={'user'}
         name={'email'}
         label={'이메일'}
         placeholder={'이메일을 입력해 주세요'}
         type={'email'}
         />
         <Input 
-        state={'user'}
         name={'email'}
         label={'비밀번호'}
         placeholder={'비밀번호를 입력해 주세요'}
