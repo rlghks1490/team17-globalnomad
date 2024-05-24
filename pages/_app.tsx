47fe570aeb5db59bc12f14b37d31b17d1a0e0dd0
rlghks1490 <rlghks1490@naver.com>
2024-05-24 17:37:22 +0900

    채널 상세 중간 pr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,17 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-<<<<<<< HEAD
-=======
   
   const queryClient = new QueryClient();
 
->>>>>>> 44bcfa21b5f4acdf92d7dabcab52d8955ef84778
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-<<<<<<< HEAD
 );
-=======
-  );
->>>>>>> 44bcfa21b5f4acdf92d7dabcab52d8955ef84778
 }
