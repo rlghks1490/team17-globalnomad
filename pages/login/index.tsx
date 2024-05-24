9505b51c7a4484b8b37b0df9e1ddc827d1bc97c5
KimTaeEun1632 <qpwoal1324@naver.com>
2024-05-24 20:57:46 +0900

    fix: 경로명 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -102,7 +102,7 @@ const Login = () => {
       <div className="mt-8 flex gap-2 text-base font-normal text-gnGray800">
         <p>회원이 아니신가요?</p>
         <Link
-          href="/signUp"
+          href="/signup"
           className="text-base font-normal text-gnDarkGreen underline"
         >
           회원가입
