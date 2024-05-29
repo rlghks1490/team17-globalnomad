19c38317f852748aeabb026a1b36f3397e578974
ryugwanghyoen <rgh4422@google.com>
2024-05-29 23:05:36 +0900

    feat:내 정보 페이지 (stash1)

---
+++ pages/myInfo/index.tsx
@@ -0,0 +1,9 @@
+import GuestHeader from "@/Components/Common/GuestHeader/GuestHeader";
+import LoginHeader from "@/Components/Common/LoginHeader/LoginHeader";
+import Footer from "@/Components/Footer/Footer";
+
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
