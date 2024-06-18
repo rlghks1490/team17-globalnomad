e560dc7f5b7a450d0d2554a5cf6d960c018f5619
KimTaeEun1632 <qpwoal1324@naver.com>
2024-06-18 15:35:08 +0900

    feat: 미들웨어

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken");
+
+  const signInUrl = new URL("/auth/sign-in", request.url);
+  const homeUrl = new URL("/", request.url);
+
+  if (accessToken && request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(homeUrl);
+  }
+
+  if (!accessToken && request.nextUrl.pathname.startsWith("/my-page")) {
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/auth/:path*", "/my-page/:path*"],
+};
