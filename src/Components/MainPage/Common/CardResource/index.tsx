bd7db928554e977b6e9aaea5d931f79d3d5935a2
jinah-dev <kimjinah.dev@gmail.com>
2024-06-21 14:16:05 +0900

    Refactor : CardResource 리팩토링

--- src/Components/MainPage/Common/CardResource/index.tsx
+++ src/Components/MainPage/Common/CardResource/index.tsx
@@ -18,21 +18,25 @@ const CardResource = ({ activitiesData, banner }: CardResourceProps) => {
   return (
     <div
       onClick={() => handleClick(activitiesData.id)}
-      className={`flex cursor-pointer flex-col gap-4 overflow-hidden ${banner ? "relative h-96 w-96 flex-col-reverse rounded-2xl bg-gradient-to-b from-gray-300 to-gray-700" : ""}`}
+      className={`${banner ? "from-#00000000 to-#000000cc relative h-[24rem] w-[24rem] cursor-pointer flex-col-reverse overflow-hidden bg-gradient-to-b mobile:h-[11.6rem] mobile:w-[11.6rem]" : "flex cursor-pointer flex-col gap-[1rem] overflow-hidden"}`}
     >
-      <div className="relative h-72 w-72 overflow-hidden rounded-2xl">
+      <div
+        className={`${banner ? "absolute left-0 top-0 z-[-1] h-full w-full overflow-hidden rounded-[1.25rem]" : "pt-full h-[17.7rem] w-[17.7rem] overflow-hidden rounded-[1.25rem] tablet:relative tablet:h-auto tablet:w-full"}`}
+      >
         <Image
           src={activitiesData.bannerImageUrl}
           width={384}
           height={384}
           alt="배너 이미지"
-          className="h-full w-full transform object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+          className={`${banner ? "absolute h-full w-full object-cover object-cover duration-300 ease-in-out hover:scale-125 hover:overflow-hidden" : "h-[17.7rem] w-[17.7rem] object-cover duration-300 ease-in-out hover:scale-125 hover:overflow-hidden tablet:absolute tablet:left-0 tablet:top-0 tablet:h-full tablet:w-full"}`}
         />
       </div>
       <div
-        className={`flex flex-col gap-4 ${banner ? "gap-8 p-6 text-white" : "w-full text-black"}`}
+        className={`${banner ? "gap-[1.25rem] pl-[1.25rem] pt-[1.88rem] text-white mobile:pl-[1.25rem] mobile:pt-[0.75rem]" : "flex w-full flex-col gap-[0.62rem] text-black"}`}
       >
-        <div className="flex items-center gap-2 text-base font-medium">
+        <div
+          className={`${banner ? "h-[1.12rem] w-[1.12rem] text-[1rem] font-semibold leading-4 text-white" : ""}`}
+        >
           <Image
             src={ICON.star.active.src}
             width={20}
@@ -46,17 +50,23 @@ const CardResource = ({ activitiesData, banner }: CardResourceProps) => {
           </span>
         </div>
         <div
-          className={`overflow-hidden text-ellipsis whitespace-nowrap text-xl font-semibold ${banner ? "w-72 text-3xl font-bold" : ""}`}
+          className={`${banner ? "text-[1.25rem] font-bold leading-[2.23rem] mobile:w-[9.12rem] mobile:text-[1.12rem] mobile:leading-[1.34rem]" : ""}`}
         >
           {activitiesData.title}
         </div>
-        <div className="flex items-center gap-2 text-2xl font-bold">
+        <div
+          className={`${banner ? "w-[18.75rem] text-[1.88rem] font-bold leading-[1.49rem] mobile:text-[1rem] mobile:leading-[1.18rem]" : ""}`}
+        >
           {activitiesData.price === 0 ? (
             "무료체험"
           ) : (
             <>
               ￦ {activitiesData.price.toLocaleString()}{" "}
-              <span className="text-lg font-normal text-gray-500">/ 인</span>
+              <span
+                className={`${banner ? "text-#a1a1a1 text-[0.88rem] font-normal" : ""}`}
+              >
+                / 인
+              </span>
             </>
           )}
         </div>
