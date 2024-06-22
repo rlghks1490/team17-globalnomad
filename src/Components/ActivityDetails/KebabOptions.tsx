f7e6f89ea983120442d5f3effb1a26b609035ed8
rlghks1490 <rlghks1490@naver.com>
2024-06-22 23:09:45 +0900

    refactor: 케밥 외부 영역 클릭 시 닫기

--- src/Components/ActivityDetails/KebabOptions.tsx
+++ src/Components/ActivityDetails/KebabOptions.tsx
@@ -21,14 +21,14 @@ const KebabOptions = ({ activityId }: KebabOptionsProps) => {
 
   return (
     <div>
-      <div className="absolute right-0 z-50 flex flex-col items-center justify-center divide-y rounded-md border border-gnGray300 bg-white shadow-[0_4px_16px_0_#112211]">
+      <div className="absolute right-0 z-50 flex flex-col items-center justify-center divide-y rounded-md border border-gnGray300 bg-white shadow-xl">
         <Link href={`/my-page/activities/${activityId}/edit`}>
-          <button className="whitespace-nowrap  px-11 py-4 text-lg font-medium">
+          <button className="whitespace-nowrap px-11  py-4 text-lg font-medium hover:bg-gnGray200">
             수정하기
           </button>
         </Link>
         <button
-          className="whitespace-nowrap px-11 py-4 text-lg font-medium"
+          className="whitespace-nowrap px-11 py-4 text-lg font-medium hover:bg-gnGray200"
           onClick={() => handleDeleteActivituies(activityId)}
         >
           삭제하기
