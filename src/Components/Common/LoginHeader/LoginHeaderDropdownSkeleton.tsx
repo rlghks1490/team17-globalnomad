84c61a7fbf0971409500da63f99ed51e54341872
ryugwanghyoen <rgh4422@google.com>
2024-06-24 17:29:28 +0900

    recommit

--- src/Components/Common/LoginHeader/LoginHeaderDropdownSkeleton.tsx
+++ src/Components/Common/LoginHeader/LoginHeaderDropdownSkeleton.tsx
@@ -4,7 +4,7 @@ const LoginHeaderDropdownSkeleton = () => {
   return (
     <div className="flex items-center justify-center gap-2">
       <div className="skeleton flex h-9 w-9 items-center justify-center rounded-full"></div>
-      <div className="skeleton w-18 flex h-5 items-center justify-center "></div>
+      <div className="skeleton w-18 flex h-5 items-center justify-center"></div>
     </div>
   );
 };
